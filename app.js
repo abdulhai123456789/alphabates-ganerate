d58f3fdf4d8230e6df1dc6bad7a7e93df1fa028d
Abdul Hai <153994789+abdulhai123456789@users.noreply.github.com>
2024-05-15 16:55:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,182 @@
+var text = document.getElementById("text")
+function a(){
+    let arr =["apple","aeroplane","ant","aero"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function b(){
+    let arr =["ball","banana","bat"];
+    var  random = Math.floor(Math.random()*3)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function c(){
+    let arr =["cat","coconet","center","corner"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function d(){
+    let arr =["dog","drum","donkey","don"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function e(){
+    let arr =["elephante","eggs","eye","egale"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function f(){
+    let arr =["fish","fanta","flower","frog","fog"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function g(){
+    let arr =["gun","game","goat","graps","gift"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function h(){
+    let arr =["hand","hen","had","house","hourse"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function i(){
+    let arr =["igloo","icecream","iron","ice","ink"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function j(){
+    let arr =["jelly","jacket","jeep","jag","jeans"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+function k(){
+    let arr =["kite","kangroo","kiwi","king","kitchen"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function l(){
+    let arr =["loin","lamp","lemon","leaf","LEG"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function m(){
+    let arr =["monkey","mango","moon","money","milk"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function n(){
+    let arr =["nail","nut","nose","nials","newspaper"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function o(){
+    let arr =["orange","owl","octopus","onian"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function p(){
+    let arr =["parrot","pagoen","popcorn","panda","park"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function q(){
+    let arr =["quak","queen","quoll","queit"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function r(){
+    let arr =["rose","road","rever","rian","rainbow"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function s(){
+    let arr =["site","sun","star","snow","shop"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function t(){
+    let arr =["tree","tiger","tea","turtle","train"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function u(){
+    let arr =["umbrella","uniform","umpire","unique"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function v(){
+    let arr =["van","vine","vase","vulture"];
+    var  random = Math.floor(Math.random()*4)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function w(){
+    let arr =["warm","watch","windowe","wand","watermalon"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
+
+function x(){
+    let arr =["x-ray","","kiwi","king","kitchen"];
+    var  random = Math.floor(Math.random()*5)
+    text.innerText = arr[random];
+    console.log(arr[random]);
+    console.log(random);
+}
